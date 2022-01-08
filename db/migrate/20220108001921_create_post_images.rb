30d080898203a9acdea39446a1972178dad0adbc
EC2 Default User <ec2-user@ip-172-31-84-89.ec2.internal>
2022-01-08 00:28:27 +0000

    [Finish] 4-8

---
+++ db/migrate/20220108001921_create_post_images.rb
@@ -0,0 +1,10 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+      t.text "shop_name"
+      t.text "caption"
+      t.integer "user_"
+      t.timestamps
+    end
+  end
+end
